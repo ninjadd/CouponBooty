4cf71889ceff5f1c928a7e46645bd1505b7fe487
Daniel Dickson <ninjadd@gmail.com>
2017-05-13 13:33:39 -0700

    Added all of the needed css & js for new template Chelsea suggested

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('type', ['admin', 'writer', 'consumer'])->default('admin');
             $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
