3c332a74a0be05f168b0cbf0a9e477a909730d27
Daniel Dickson <ninjadd@gmail.com>
2017-05-21 18:01:54 -0700

    fixed some welcome page styling issues

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,30 +3,34 @@
 @section('content')
 
     <div class="container">
-        <div class="jumbotron">
-            <h1>CouponBooty</h1>
-            <p>This is an example to show the potential of how awesome coupons can be in your booty.</p>
-        </div>
-        <div class="row">
-            @if(count($offers))
-                @foreach($offers as $offer)
-                    <div class="col-md-3">
-                        <div class="panel panel-info">
-                            <div class="panel-heading">
-                                <h4 class="panel-title">{{ $offer->title }}</h4>
-                            </div>
-                            <div class="panel-body">
-                                {!! $offer->body !!}
-                            </div>
-                            <div class="panel-footer">
-                                <a class="btn btn-primary" href="#" role="button">View Coupon &raquo;</a>
+        <div class="col-md-12">
+            <div class="jumbotron">
+                <h1>CouponBooty</h1>
+                <p>This is an example to show the potential of how awesome coupons can be in your booty.</p>
+            </div>
+            <div class="row">
+                @if(count($offers))
+
+                    @foreach($offers as $offer)
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">{{ $offer->title }}</h4>
+                                </div>
+                                <div class="panel-body" style="min-height: 100; max-height: 100;">
+                                    {!! $offer->body !!}
+                                </div>
+                                <div class="panel-footer">
+                                    <a class="btn btn-primary" href="#" role="button">View Coupon &raquo;</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            @endif
-        </div><!--/row-->
-        {{ $offers->links() }}
+                    @endforeach
+
+                @endif
+            </div><!--/row-->
+            {{ $offers->links() }}
+        </div>
     </div>
 
 @endsection
