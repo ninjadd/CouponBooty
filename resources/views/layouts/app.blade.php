2f713667f3ecc28184eba69f714f3d5ca2d49728
Daniel <ninjadd@gmail.com>
2017-06-08 13:19:00 -0700

    some updates for the front page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
                         <div class="form-group">
                             <input type="text" name="search_text" required="required" class="form-control" placeholder="Search">
                         </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
+                        <button type="submit" class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i></button>
                     </form>
                     <li>
                         <a class="fa fa-facebook-square" aria-hidden="true" href="https://www.facebook.com/CouponBooty/" target="_blank"></a>
