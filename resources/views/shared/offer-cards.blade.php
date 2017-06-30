44a0418499a729ca951c7cc069a0da006c73b27e
Daniel <ninjadd@gmail.com>
2017-06-29 23:01:00 -0700

    small text changes on cards

--- resources/views/shared/offer-cards.blade.php
+++ resources/views/shared/offer-cards.blade.php
@@ -4,14 +4,14 @@
             <div class="card-image waves-effect waves-block waves-light">
                 <img class="activator" src="{{ $offer->image_url }}">
             </div>
-            <div class="card-content valign-wrapper">
-                <span class="card-title activator grey-text text-darken-4 flow-text">{{ $offer->title }}<i class="material-icons right">more_vert</i></span>
+            <div class="card-content">
+                <h6 class="card-title activator grey-text text-darken-4">{{ $offer->title }}<i class="material-icons right">more_vert</i></h6>
             </div>
             <div class="card-action">
                 <a class="waves-effect waves-light btn deep-orange" href="{{ $offer->url }}" target="_blank">Get Deal</a>
             </div>
             <div class="card-reveal">
-                <span class="card-title grey-text text-darken-4 flow-text">{{ $offer->title }}<i class="material-icons right">close</i></span>
+                <span class="card-title grey-text text-darken-4">{{ $offer->title }}<i class="material-icons right">close</i></span>
                 @if($offer->coupon)
                     <br>
                     COPY:
@@ -20,17 +20,17 @@
                     <br>
                     <a class="waves-effect waves-light deep-purple btn" href="{{ $offer->url }}" target="_blank">USE COUPON</a>
                     <br>
-                    <p class="flow-text">{{ strip_tags($offer->body) }}</p>
+                    <p>{{ strip_tags($offer->body) }}</p>
                 @else
-                    <p class="flow-text">{{ strip_tags($offer->body) }}</p>
+                    <p>{{ strip_tags($offer->body) }}</p>
                     <a class="waves-effect waves-light btn deep-orange" href="{{ $offer->url }}" target="_blank">Get Deal</a>
                 @endif
                 <br>
                 <a href="https://twitter.com/home?status={{ $offer->url }}" target="_blank">
-                    <i class="fa fa-twitter-square fa-3x blue-grey-text" aria-hidden="true"></i>
+                    <i class="fa fa-twitter-square fa-2x blue-grey-text" aria-hidden="true"></i>
                 </a>
                 <a href="https://www.facebook.com/sharer/sharer.php?u={{ $offer->url }}" target="_blank">
-                    <i class="fa fa-facebook-square fa-3x blue-grey-text" aria-hidden="true"></i>
+                    <i class="fa fa-facebook-square fa-2x blue-grey-text" aria-hidden="true"></i>
                 </a>
             </div>
         </div>
