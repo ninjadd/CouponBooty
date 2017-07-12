c0de3a8b8512e126449c52b852fea3870ed6e4d4
Daniel <ninjadd@gmail.com>
2017-07-12 14:36:14 -0700

    new lay out for store-slugs

--- resources/views/pages/store-slug.blade.php
+++ resources/views/pages/store-slug.blade.php
@@ -36,7 +36,72 @@
         @endif
 
         <div class="row">
-            @include('shared.offer-cards')
+            <div class="col s12 m8 offset-m2 l6 offset-l3">
+                <div class="card-panel z-depth-4">
+                    <div class="row valign-wrapper">
+                        <div class="col 12">
+                            <img src="{{ $store->image_url }}" alt="{{ $store->name }}" class="responsive-img">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            @foreach($offers as $offer)
+                <div class="card small hoverable">
+                    <div class="card-content">
+                        <h6 class="card-title activator grey-text text-darken-4">{{ $offer->title }}<i class="material-icons right">more_vert</i></h6>
+                        <p>{{ strip_tags($offer->body) }}</p>
+                        @if($offer->end_date)
+                            <p>Ends: {{ $offer->end_date }}</p>
+                        @endif
+
+                        <p>
+                            <br>
+                            <a href="https://twitter.com/home?status={{ urlencode($offer->url) }}" target="_blank">
+                                <i class="fa fa-twitter-square fa-2x blue-grey-text" aria-hidden="true"></i>
+                            </a>
+                            <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($offer->url) }}" target="_blank">
+                                <i class="fa fa-facebook-square fa-2x blue-grey-text" aria-hidden="true"></i>
+                            </a>
+                        </p>
+                    </div>
+                    <div class="card-action">
+                        @if($offer->coupon)
+                            <a class="waves-effect activator waves-light btn deep-purple">See Details</a>
+                        @else
+                            <a class="waves-effect waves-light btn deep-orange" href="{{ $offer->url }}" target="_blank">Get Deal</a>
+                        @endif
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title grey-text text-darken-4">{{ $offer->title }}<i class="material-icons right">close</i></span>
+                        @if($offer->coupon)
+                            <br>
+                            COPY:
+                            <br>
+                            <h5 class="z-depth-1-half teal-text text-darken-3 bold center-align">{{ $offer->coupon }}</h5>
+                            <br>
+                            <a class="waves-effect waves-light deep-purple btn" href="{{ $offer->url }}" target="_blank">USE COUPON</a>
+                            <p>{{ strip_tags($offer->body) }}</p>
+                        @else
+                            <br>
+                            <a class="waves-effect waves-light btn deep-orange" href="{{ $offer->url }}" target="_blank">Get Deal</a>
+                            <p>{{ strip_tags($offer->body) }}</p>
+                        @endif
+                        @if($offer->end_date)
+                            <p>Ends: {{ $offer->end_date }}</p>
+                        @endif
+
+                        <a href="https://twitter.com/home?status={{ urlencode($offer->url) }}" target="_blank">
+                            <i class="fa fa-twitter-square fa-2x blue-grey-text" aria-hidden="true"></i>
+                        </a>
+                        <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode($offer->url) }}" target="_blank">
+                            <i class="fa fa-facebook-square fa-2x blue-grey-text" aria-hidden="true"></i>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 
