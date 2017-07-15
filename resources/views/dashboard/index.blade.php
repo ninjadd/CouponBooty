9ab5425d463ab04a72ee79de14201d8a5e5ded06
Daniel <ninjadd@gmail.com>
2017-07-15 14:00:05 -0700

    Huge bug small fix on dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -67,7 +67,7 @@
                                             {{ (!empty($offer->end_date)) ? $offer->end_date : '' }}
                                         </td>
                                         <td>
-                                            <span class="hidden" ">{{ $offer->id }}</div>
+                                            <div class="hidden">{{ $offer->id }}</div>
                                             <form action="/offer/{{ $offer->id }}" method="POST">
                                                 <div class="btn-group" role="group" aria-label="...">
                                                 <a href="/offer/{{ $offer->id }}" type="button" class="btn btn-default btn-xs" data-toggle="tooltip" title="View Offer">
