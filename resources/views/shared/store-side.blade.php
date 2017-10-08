fe271806b9b0c80609dc36e4ef20fefb9592b394
Daniel <ninjadd@gmail.com>
2017-10-08 15:04:45 -0700

    fixed bug in store-side list and updated composer

--- resources/views/shared/store-side.blade.php
+++ resources/views/shared/store-side.blade.php
@@ -3,6 +3,6 @@
 </ul>
 <div class="collection">
     @foreach($stores as $store)
-        <a href="/view/{{ $store->slug }}" class="collection-item purple-text text-lighten-1"><span class="badge purple lighten-1 white-text">{{ $store->offers->count() }}</span>{{ $store->name }}</a>
+        <a href="/view/{{ $store->slug }}" class="collection-item purple-text text-lighten-1"><span class="badge purple lighten-1 white-text">{{ $store->offers->where('archive', '=', 0)->count() }}</span>{{ $store->name }}</a>
     @endforeach
 </div>
